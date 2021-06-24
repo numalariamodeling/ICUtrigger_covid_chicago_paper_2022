7de7620301b770533cb7ee3c75665ce455f664a9
ManuelaRunge <manuela.runge@northwestern.edu>
2021-06-24 02:08:02 -0500

    reformat code

--- figures/Fig5B.R
+++ figures/Fig5B.R
@@ -5,30 +5,30 @@ source(file.path('setup/settings.R'))
 source(file.path('setup/helper_functions.R'))
 customTheme <- f_getCustomTheme()
 trace_selection = TRUE
-if(trace_selection) fig_dir = fig_dir_traces
+if (trace_selection) fig_dir = fig_dir_traces
 
-f_combineData <- function(exp_names, sim_end_date,trace_selection){
+f_combineData <- function(exp_names, sim_end_date, trace_selection) {
   dat_list <- list()
-  for(exp_name in exp_names ){
+  for (exp_name in exp_names) {
     print(exp_name)
-    tempdat <- f_load_sim_data(exp_name=exp_name,sim_dir = sim_dir ,
-                               fname="trajectoriesDat_region_11_trimfut.csv",
-                               add_peak_cols=TRUE,addRt=FALSE, trace_selection=trace_selection) %>%
-      filter(date>=baseline_date &  date <= sim_end_date) %>%
+    tempdat <- f_load_sim_data(exp_name = exp_name, sim_dir = sim_dir,
+                               fname = "trajectoriesDat_region_11_trimfut.csv",
+                               add_peak_cols = TRUE, addRt = FALSE, trace_selection = trace_selection) %>%
+      filter(date >= baseline_date & date <= sim_end_date) %>%
       dplyr::group_by(exp_name, group_id) %>%
-      filter(trigger_activated==1 & date==date_peak) %>%
+      filter(trigger_activated == 1 & date == date_peak) %>%
       dplyr::select(exp_name, group_id, sample_num, scen_num, capacity_multiplier, reopen, rollback, delay,
-                    date,date_peak, trigger_activated,triggerDate , crit_det, crit_det_peak)
+                    date, date_peak, trigger_activated, triggerDate, crit_det, crit_det_peak)
 
-    groupVars=c('rollback','delay','reopen', 'capacity_multiplier')
-    dat_prob_raw   <-  f_sample_trajectories(tempdat,groupVars=groupVars)
-    dat_list[[length(dat_list)+1]]  <- dat_prob_raw %>%
+    groupVars = c('rollback', 'delay', 'reopen', 'capacity_multiplier')
+    dat_prob_raw <- f_sample_trajectories(tempdat, groupVars = groupVars)
+    dat_list[[length(dat_list) + 1]] <- dat_prob_raw %>%
       dplyr::group_by_at(groupVars) %>%
-      dplyr::summarize(n.val 		= n(),
-                       prob_lower= min(perc_above),
+      dplyr::summarize(n.val = n(),
+                       prob_lower = min(perc_above),
                        prob_upper = max(perc_above),
-                       prob_median=median(perc_above),
-                       prob=mean(perc_above))
+                       prob_median = median(perc_above),
+                       prob = mean(perc_above))
 
     rm(tempdat)
   }
@@ -38,94 +38,94 @@ f_combineData <- function(exp_names, sim_end_date,trace_selection){
   return(dat)
 }
 
-p5Bdat <- f_combineData(exp_names = c(exp_names_50_delay1,exp_names_100_delay1,
-                                      exp_names_50_delay7,exp_names_100_delay7),
-                        sim_end_date=sim_end_date,
-                        trace_selection=trace_selection)
+p5Bdat <- f_combineData(exp_names = c(exp_names_50_delay1, exp_names_100_delay1,
+                                      exp_names_50_delay7, exp_names_100_delay7),
+                        sim_end_date = sim_end_date,
+                        trace_selection = trace_selection)
 
 
 p5Bdat$rollback <- factor(p5Bdat$rollback,
-                          levels=c("pr2","pr4","pr6","pr8"),
-                          labels=c("weak (20%)","moderate (40%)","strong (60%)","very strong (80%)"))
+                          levels = c("pr2", "pr4", "pr6", "pr8"),
+                          labels = c("weak (20%)", "moderate (40%)", "strong (60%)", "very strong (80%)"))
 
 p5Bdat$delay <- factor(p5Bdat$delay,
-                       levels=c("1daysdelay","7daysdelay"),
-                       labels=c("immediate mitigation:\n1 day after threshold reached",
-                                "delayed mitigation:\n7 days after threshold reached"))
+                       levels = c("1daysdelay", "7daysdelay"),
+                       labels = c("immediate mitigation:\n1 day after threshold reached",
+                                  "delayed mitigation:\n7 days after threshold reached"))
 
 p5Bdat$reopen <- factor(p5Bdat$reopen,
-                        levels=c("100perc","50perc"),
-                        labels=c("High increase in transmission:\nRt approx. 1.25",
-                                 "Low increase in transmission:\nRt approx. 1.15"))
+                        levels = c("100perc", "50perc"),
+                        labels = c("High increase in transmission:\nRt approx. 1.28",
+                                   "Low increase in transmission:\nRt approx. 1.16"))
 
-p5B <- f_prob_plot_base(dat = subset(p5Bdat, rollback!="counterfactual")) +
-  facet_grid(reopen ~ delay)+
-  geom_hline(yintercept = 0.25, col='darkgrey',size=0.8)
+p5B <- f_prob_plot_base(dat = subset(p5Bdat, rollback != "counterfactual")) +
+  facet_grid(reopen ~ delay) +
+  geom_hline(yintercept = 0.25, col = 'darkgrey', size = 0.8)
 #print(p5B)
 
-fwrite(p5Bdat, file.path(fig_dir,"csv","p5Bdat.csv"))
+fwrite(p5Bdat, file.path(fig_dir, "csv", "p5Bdat.csv"))
 
 f_save_plot(
   plot_name = paste0("Fig5B"), pplot = p5B,
-  plot_dir = file.path(fig_dir), width =12, height =10
-  )
+  plot_dir = file.path(fig_dir), width = 12, height = 10
+)
 
 ### For text
- p5Bdat %>%
-   filter(delay=="immediate mitigation:\n1 day after threshold reached") %>%
-   ungroup() %>%
-   select(reopen,delay, rollback, capacity_multiplier,prob) %>%
-   pivot_wider(names_from = reopen, values_from = prob) %>%
-   mutate(prob_diff = `High increase in transmission:\nRt approx. 1.25`-
-     `Low increase in transmission:\nRt approx. 1.15`) %>%
-   group_by(delay) %>%
-   summarize(mean=mean(prob_diff,na.rm = TRUE),
-             q5 = round(quantile(prob_diff, probs = 0.05, na.rm = TRUE),1),
-             q95 = round(quantile(prob_diff, probs = 0.95, na.rm = TRUE),1))
-
- p5Bdat %>%
-   filter(delay=="immediate mitigation:\n1 day after threshold reached") %>%
-   ungroup() %>%
-   select(reopen,delay, rollback, capacity_multiplier,prob) %>%
-   pivot_wider(names_from = reopen, values_from = prob) %>%
-   mutate(prob_diff = `High increase in transmission:\nRt approx. 1.25`-
-     `Low increase in transmission:\nRt approx. 1.15`) %>%
-   group_by(delay, rollback) %>%
-   summarize(mean=mean(prob_diff,na.rm = TRUE),
-             q5 = round(quantile(prob_diff, probs = 0.05, na.rm = TRUE),1),
-             q95 = round(quantile(prob_diff, probs = 0.95, na.rm = TRUE),1))
-
- p5Bdat %>%
-   filter(delay=="immediate mitigation:\n1 day after threshold reached") %>%
-   ungroup() %>%
-   select(reopen,delay, rollback, capacity_multiplier,prob) %>%
-   group_by(reopen, rollback) %>%
-   summarize(mean=mean(prob,na.rm = TRUE),
-             q5 = round(quantile(prob, probs = 0.05, na.rm = TRUE),1),
-             q95 = round(quantile(prob, probs = 0.95, na.rm = TRUE),1))
+p5Bdat %>%
+  filter(delay == "immediate mitigation:\n1 day after threshold reached") %>%
+  ungroup() %>%
+  select(reopen, delay, rollback, capacity_multiplier, prob) %>%
+  pivot_wider(names_from = reopen, values_from = prob) %>%
+  mutate(prob_diff = `High increase in transmission:\nRt approx. 1.28` -
+    `Low increase in transmission:\nRt approx. 1.16`) %>%
+  group_by(delay) %>%
+  summarize(mean = mean(prob_diff, na.rm = TRUE),
+            q5 = round(quantile(prob_diff, probs = 0.05, na.rm = TRUE), 1),
+            q95 = round(quantile(prob_diff, probs = 0.95, na.rm = TRUE), 1))
 
 p5Bdat %>%
-  filter(delay=="immediate mitigation:\n1 day after threshold reached" ) %>%
-  group_by(reopen,delay, rollback) %>%
-  summarize(mean=mean(prob),
-            prob_lower=mean(prob_lower),
-            prob_upper=mean(prob_upper))
+  filter(delay == "immediate mitigation:\n1 day after threshold reached") %>%
+  ungroup() %>%
+  select(reopen, delay, rollback, capacity_multiplier, prob) %>%
+  pivot_wider(names_from = reopen, values_from = prob) %>%
+  mutate(prob_diff = `High increase in transmission:\nRt approx. 1.28` -
+    `Low increase in transmission:\nRt approx. 1.16`) %>%
+  group_by(delay, rollback) %>%
+  summarize(mean = mean(prob_diff, na.rm = TRUE),
+            q5 = round(quantile(prob_diff, probs = 0.05, na.rm = TRUE), 1),
+            q95 = round(quantile(prob_diff, probs = 0.95, na.rm = TRUE), 1))
+
+p5Bdat %>%
+  filter(delay == "immediate mitigation:\n1 day after threshold reached") %>%
+  ungroup() %>%
+  select(reopen, delay, rollback, capacity_multiplier, prob) %>%
+  group_by(reopen, rollback) %>%
+  summarize(mean = mean(prob, na.rm = TRUE),
+            q5 = round(quantile(prob, probs = 0.05, na.rm = TRUE), 1),
+            q95 = round(quantile(prob, probs = 0.95, na.rm = TRUE), 1))
+
+p5Bdat %>%
+  filter(delay == "immediate mitigation:\n1 day after threshold reached") %>%
+  group_by(reopen, delay, rollback) %>%
+  summarize(mean = mean(prob),
+            prob_lower = mean(prob_lower),
+            prob_upper = mean(prob_upper))
 
 
 p5Bdat %>%
-  filter(reopen=="High increase in transmission:\nRt approx. 1.25" &
-         rollback=="moderate (40%)") %>%
-  group_by(reopen,delay, rollback) %>%
-  summarize(mean=mean(prob),
-            prob_lower=mean(prob_lower),
-            prob_upper=mean(prob_upper))
+  filter(reopen == "High increase in transmission:\nRt approx. 1.28" &
+           rollback == "moderate (40%)") %>%
+  group_by(reopen, delay, rollback) %>%
+  summarize(mean = mean(prob),
+            prob_lower = mean(prob_lower),
+            prob_upper = mean(prob_upper))
 
 p5Bdat %>%
-  filter(reopen!="High increase in transmission:\nRt approx. 1.25" &
-         rollback=="very strong (80%)" &
-  capacity_multiplier==0.8)
+  filter(reopen != "High increase in transmission:\nRt approx. 1.28" &
+           rollback == "very strong (80%)" &
+           capacity_multiplier == 0.8)
 
 p5Bdat %>%
-  filter(reopen=="High increase in transmission:\nRt approx. 1.25" &
-         rollback=="very strong (80%)" &
-  capacity_multiplier==0.6)
+  filter(reopen == "High increase in transmission:\nRt approx. 1.28" &
+           rollback == "very strong (80%)" &
+           capacity_multiplier == 0.6)
--- figures/S1_table_6.R
+++ figures/S1_table_6.R
@@ -6,69 +6,69 @@ source(file.path('setup/helper_functions.R'))
 customTheme <- f_getCustomTheme()
 
 trace_selection <- TRUE
-capacity_multipliers <-  c(0.4, 0.6, 0.8,1)
+capacity_multipliers <- c(0.4, 0.6, 0.8, 1)
 exp_names <- list.dirs(sim_dir, full.names = FALSE)
-exp_names <- exp_names[grep("triggeredrollback_reopen",exp_names)]
-exp_names <- exp_names[!grepl("14daysdelay",exp_names)]
+exp_names <- exp_names[grep("triggeredrollback_reopen", exp_names)]
+exp_names <- exp_names[!grepl("14daysdelay", exp_names)]
 
-column_names <- c("ICU_peak", "ICU_peak_date",  "trigger_to_peak",
+column_names <- c("ICU_peak", "ICU_peak_date", "trigger_to_peak",
                   "ICUoverflow", "ICUoverflow_date", "trigger_to_overflow")
-scen_table <- matrix(NA, length(exp_names)*length(capacity_multipliers), length(column_names)+2)
-colnames(scen_table) <- c("exp_name","capacity_multiplier",column_names)
+scen_table <- matrix(NA, length(exp_names) * length(capacity_multipliers), length(column_names) + 2)
+colnames(scen_table) <- c("exp_name", "capacity_multiplier", column_names)
 scen_table <- as.data.frame(scen_table)
 
 dat_list <- list()
-for(exp_name in exp_names){
+for (exp_name in exp_names) {
   print(exp_name)
-  dat_list[[length(dat_list)+1]] <- f_load_sim_data(
-    exp_name,fname="trajectoriesDat_region_11_trimfut.csv", sim_dir,
-    add_peak_cols=TRUE, add_trigger_cols=TRUE,addRt=FALSE,trace_selection=trace_selection) %>%
-    filter(date <= sim_end_date & date_peak <= last_plot_date & capacity_multiplier %in% capacity_multipliers) %>%
+  dat_list[[length(dat_list) + 1]] <- f_load_sim_data(
+    exp_name, fname = "trajectoriesDat_region_11_trimfut.csv", sim_dir,
+    add_peak_cols = TRUE, add_trigger_cols = TRUE, addRt = FALSE, trace_selection = trace_selection) %>%
+    filter(date <= sim_end_date &
+             date_peak <= last_plot_date &
+             capacity_multiplier %in% capacity_multipliers) %>%
     dplyr::group_by(exp_name, group_id) %>%
-    dplyr::mutate(rel_occupancy = crit_det/516,
-                  rel_occupancy_peak = crit_det_peak/516,
+    dplyr::mutate(rel_occupancy = crit_det / 516,
+                  rel_occupancy_peak = crit_det_peak / 516,
                   time_since_reopen = as.numeric(date - baseline_date),
-                  time_to_peak_after_reopen = as.numeric( date_peak - baseline_date),
-                  time_to_trigger_after_reopen = as.numeric( triggerDate - baseline_date),
-                  time_since_trigger = round(as.numeric(date - triggerDate),0),
-                  time_to_peak_after_trigger = round( as.numeric( date_peak - triggerDate),0)) %>%
-    dplyr::filter(trigger_activated==1) %>%
+                  time_to_peak_after_reopen = as.numeric(date_peak - baseline_date),
+                  time_to_trigger_after_reopen = as.numeric(triggerDate - baseline_date),
+                  time_since_trigger = round(as.numeric(date - triggerDate), 0),
+                  time_to_peak_after_trigger = round(as.numeric(date_peak - triggerDate), 0)) %>%
+    dplyr::filter(trigger_activated == 1) %>%
     ungroup() %>%
-    dplyr::select(exp_name, capacity_multiplier,rel_occupancy_peak, time_to_peak_after_reopen,
-                  time_to_trigger_after_reopen, time_to_peak_after_trigger ) %>%
-    pivot_longer(cols=-c(exp_name, capacity_multiplier), names_to = "outcome") %>%
+    dplyr::select(exp_name, capacity_multiplier, rel_occupancy_peak, time_to_peak_after_reopen,
+                  time_to_trigger_after_reopen, time_to_peak_after_trigger) %>%
+    pivot_longer(cols = -c(exp_name, capacity_multiplier), names_to = "outcome") %>%
     dplyr::group_by(exp_name, capacity_multiplier, outcome) %>%
-    dplyr::summarise(n=n(),
+    dplyr::summarise(n = n(),
                      median = median(value),
-                     lower = quantile(value, probs=0.05, na.rm = TRUE),
-                     upper = quantile(value, probs=0.95, na.rm = TRUE))
+                     lower = quantile(value, probs = 0.05, na.rm = TRUE),
+                     upper = quantile(value, probs = 0.95, na.rm = TRUE))
 }
 
-dat <- dat_list %>% bind_rows() %>%
-  filter(outcome!="group_id") %>%
-  mutate(exp_name = gsub("20210517_IL_localeEMS_11_","", exp_name),
-         exp_name = gsub("20210518_IL_localeEMS_11_","", exp_name),
-         exp_name = gsub("20210527_IL_localeEMS_11_","", exp_name),
-         exp_name = gsub("20210528_IL_localeEMS_11_","", exp_name),
-         exp_name = gsub("_triggeredrollback_reopen","", exp_name)) %>%
-  separate(exp_name , into= c("reopen", "delay", "mitigation"), sep = "_")
+dat <- dat_list %>%
+  bind_rows() %>%
+  filter(outcome != "group_id") %>%
+  mutate(exp_name = exp_name) %>%
+  separate(exp_name, into = c("reopen", "delay", "mitigation"), sep = "_")
 
 dat$mitigation <- factor(dat$mitigation,
-                         levels=c("pr2","pr4","pr6","pr8"),
-                         labels=c("weak (20%)","moderate (40%)","strong (60%)","very strong (80%)"))
+                         levels = c("pr2", "pr4", "pr6", "pr8"),
+                         labels = c("weak (20%)", "moderate (40%)", "strong (60%)", "very strong (80%)"))
 
 dat$delay <- factor(dat$delay,
-                    levels=c("1daysdelay","7daysdelay"),
-                    labels=c("1 day","7 days"))
+                    levels = c("1daysdelay", "7daysdelay"),
+                    labels = c("1 day", "7 days"))
 
 dat$reopen <- factor(dat$reopen,
-                     levels=c("100perc","50perc"),
-                     labels=c("High","Low"))
+                     levels = c("100perc", "50perc"),
+                     labels = c("High", "Low"))
 
-dat <- dat %>% pivot_wider(names_from=outcome,
-                           values_from = c('median' , 'lower',   'upper'),
-                           names_glue = "{outcome}_{.value}") %>%
+dat <- dat %>%
+  pivot_wider(names_from = outcome,
+              values_from = c('median', 'lower', 'upper'),
+              names_glue = "{outcome}_{.value}") %>%
   select(order(colnames(.))) %>%
   select(reopen, mitigation, delay, capacity_multiplier, everything())
 
-fwrite(dat, file.path(git_dir,"out", "S1_table_6.csv"))
+fwrite(dat, file.path(git_dir, "out", "S1_table_6.csv"))
